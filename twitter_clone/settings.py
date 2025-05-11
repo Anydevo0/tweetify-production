aabe6f1e1d92979b96ddfb936a00045a70fa0c76
Devarsh <devarsh.chhatrala@gmail.com>
2025-05-11 21:28:17 +0530

    Added * into allowrd_hosts in settings

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tweetify.onrender.com', 'www.tweetify.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
